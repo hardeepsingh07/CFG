455f6cb358c5f11c2e799bc82f0bb849a07cfb0d
Hardeep Singh <hardeep07@yahoo.com>
2016-04-08 17:16:53 -0700

    Progress with if and else loop

--- src/CFG.java
+++ src/CFG.java
@@ -11,101 +11,90 @@ import javax.swing.*;
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
 
-public class CFG extends Applet{
+public class CFG extends Applet {
 
 	public static Scanner sc = null;
-	public static String mainData = "";
-	public static String ifSt = "";
-	public static String ifData = "";
-	public static String elseSt = "";
-	public static String elseData = "";
-	public static String whileSt = "";
-	public static String whileData = "";
-	public static String line;
-	public static DirectedGraph<String, DefaultEdge> graph =
-            new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+	public static String mainData, ifSt, ifData, elseSt, elseData, whileSt, whileData, line, previous;
+	public static DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(
+			DefaultEdge.class);
 
 	public static void main(String[] args) throws Exception {
 		sc = new Scanner(new File("testing.txt"));
-		
-		while(sc.hasNextLine()) {
+
+		while (sc.hasNextLine()) {
 			line = sc.nextLine();
-			if(line.contains("if") || line.contains("while")) {
-				graph.addVertex(mainData);
+			if (line.contains("if") || line.contains("while") || line.contains("else")) {
 				if(line.contains("if")) {
+					graph.addVertex(line);
+					graph.addEdge(previous, line);
+					
+					//keep instance of if to attach to else if found later
 					ifSt = line;
-					graph.addVertex(ifSt);
-					graph.addEdge(mainData, ifSt);
+					
+					//keep track of previous to move forward
+					previous = line;
+					
+					//move through if data
 					while(!((line = sc.nextLine()).trim().equals("}"))) {
-						ifData += "\n" + line;
+						graph.addVertex(line);
+						graph.addEdge(previous, line);
+						previous = line;
 					}
-					graph.addVertex(ifData);
-					graph.addEdge(ifSt, ifData);
-					if((line = sc.nextLine()).trim().contains("else")) {
-						elseSt = line;
-						graph.addVertex(elseSt);
-						graph.addEdge(mainData, elseSt);
-						while(!((line = sc.nextLine()).trim().equals("}"))) {
-							elseData += line;
-						}
-						graph.addVertex(elseData);
-						graph.addEdge(elseSt, elseData);
-					}
-				} else if (line.contains("while")) {
-					whileSt = line;
-					graph.addVertex(whileSt);
-					graph.addEdge(mainData, whileSt);
+				} else if (line.contains("else")) {
+					previous = ifSt;
 					while(!((line = sc.nextLine()).trim().equals("}"))) {
-						whileData += line;
+						graph.addVertex(line);
+						graph.addEdge(previous, line);
+						previous = line;
 					}
-					graph.addVertex(whileData);
-					graph.addEdge(whileSt, whileData);
-					graph.addEdge(whileData, whileSt);
 				}
 			} else {
-				mainData += line;
+				if (previous == null) {
+					graph.addVertex(line);
+					previous = line;
+				} else {
+					graph.addVertex(line);
+					graph.addEdge(previous, line);
+					previous = line;
+				}
 			}
-			//How to tack on the last main????
 		}
-		
-		
-		
-//		while (sc.hasNextLine()) {
-//			line = sc.nextLine();
-//			if (line.contains("if")) {
-//				parseIf(line);
-//			} else if (line.contains("else")) {
-//				parseElse(line);
-//			} else if (line.contains("while")) {
-//				parseWhile(line);
-//			} else {
-//				parseMain(line);
-//			}
-//		}
-//		System.out.println(mainData);
-//		System.out.println(ifSt);
-//		System.out.println(ifData);
-//		System.out.println(elseSt);
-//		System.out.println(elseData);
-//		System.out.println(whileSt);
-//		System.out.println(whileData);
-		
+
+		// while (sc.hasNextLine()) {
+		// line = sc.nextLine();
+		// if (line.contains("if")) {
+		// parseIf(line);
+		// } else if (line.contains("else")) {
+		// parseElse(line);
+		// } else if (line.contains("while")) {
+		// parseWhile(line);
+		// } else {
+		// parseMain(line);
+		// }
+		// }
+		// System.out.println(mainData);
+		// System.out.println(ifSt);
+		// System.out.println(ifData);
+		// System.out.println(elseSt);
+		// System.out.println(elseData);
+		// System.out.println(whileSt);
+		// System.out.println(whileData);
+
 		Graph<String, DefaultEdge> g = graph;
-    	boolean selfReferencesAllowed = false;
-    	
-    	JFrame frame = new JFrame();
-    	frame.getContentPane().add(
-    			new GraphPanel<String, DefaultEdge>(g, selfReferencesAllowed));
-    	frame.setPreferredSize(new Dimension(1000, 1000));
-    	frame.setTitle("Context Flow Graph");
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.pack();
-    	frame.setLocationRelativeTo(null);
-    	frame.setVisible(true);
-    	try {
-    		Thread.sleep(5000000);
-    	} catch (InterruptedException ex) {
-    	}
+		boolean selfReferencesAllowed = false;
+
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new GraphPanel<String, DefaultEdge>(g, selfReferencesAllowed));
+		frame.setPreferredSize(new Dimension(1000, 1000));
+		frame.setTitle("Context Flow Graph");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		try {
+			Thread.sleep(5000000);
+		} catch (InterruptedException ex) {
+		}
 	}
 
 	public static void parseMain(String s) {
@@ -133,7 +122,7 @@ public class CFG extends Applet{
 		while (!((line = sc.nextLine()).trim().equals("}"))) {
 			if (line.contains("while")) {
 				parseWhile(line);
-			} else if(line.contains("if")) {
+			} else if (line.contains("if")) {
 				parseIf(line);
 			} else {
 				elseData += line.trim();
@@ -146,23 +135,20 @@ public class CFG extends Applet{
 		while (!((line = sc.nextLine()).trim().equals("}"))) {
 			if (line.contains("if")) {
 				parseIf(line);
-			} else if(line.contains("while")) {
+			} else if (line.contains("while")) {
 				parseWhile(line);
-			} else if(line.contains("else")) {
-				parseElse(line);			
+			} else if (line.contains("else")) {
+				parseElse(line);
 			} else {
 				whileData += line.trim();
 			}
 		}
 	}
-	
-	public void init()
-    {
-        boolean selfReferencesAllowed = false;
-        setLayout(new BorderLayout());
-        setSize(500, 500);
-        add(
-            new GraphPanel<String, DefaultEdge>(graph, selfReferencesAllowed),
-            BorderLayout.CENTER);
-    }
+
+	public void init() {
+		boolean selfReferencesAllowed = false;
+		setLayout(new BorderLayout());
+		setSize(500, 500);
+		add(new GraphPanel<String, DefaultEdge>(graph, selfReferencesAllowed), BorderLayout.CENTER);
+	}
 }
